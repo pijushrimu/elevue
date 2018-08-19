27c2f8eb64885f0e470276ebf91a8d14a1c20cc8
pijush <jaiaiassom@gmail.com>
2018-08-19 11:27:15 +0530

    removed animation

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="app">
     <app-landing-page v-if="$route.path !== '/print' && $route.path !== '/editSales' && $route.path !== '/editPurchase' && $route.path !== '/editStock'"></app-landing-page>
-    <transition enter-active-class="animated fadeIn" leave-active-class="animated fadeOut" mode="out-in">
+    <!-- <transition enter-active-class="animated fadeIn" leave-active-class="animated fadeOut" mode="out-in"> -->
       <router-view></router-view>
-    </transition>
+    <!-- </transition> -->
   </div>
 </template>
 
